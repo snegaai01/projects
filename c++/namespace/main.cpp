b96cdce883c6276fc02f9904026be0a75f86dfbc
Snega D <Snegaai01@brightskills.com>
2024-02-01 13:47:20 +0530

    files

---
+++ c++/namespace/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+namespace student
+{
+string name="Tamil";
+}
+int main()
+{
+
+    string name ="Tamil";
+    cout << name << endl;
+    string name1 ="Tamil";
+    cout << name1 << endl;
+    return 0;
+}
+
+
