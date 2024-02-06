a8bc8095917f78916ff2b4ca27898b484b5ae808
Snega D <Snegaai01@brightskills.com>
2024-02-06 11:48:04 +0530

    pyhton file

---
+++ python/pythondemo/first.py
@@ -0,0 +1,33 @@
+
+print("hello world");
+x=10;
+print(x);
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+  txt= "snega is a snake"
+  if "snake" in txt:
+  	print("yes snake' is present");
+  else:
+  	print("no, 'snake' is not present");
+  d=10;
+  b=20;
+  c=d+b;
+  print(c);
+
+g=str("snega");
+print(g);
+h=int(6);
+print(h);
+
+print(type(x));
+
+n=int(input("Enter a number:"))
+factorial=n
+
+if n >=1:
+  for i in range(1,n+1):
+    factorial=factorial*i
+    print("Factorial of given number is :",factorial)
+
+
