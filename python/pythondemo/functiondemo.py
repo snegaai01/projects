b56d427cfef29bf3e3b70c83e4386a47b05b493d
Snega D <Snegaai01@brightskills.com>
2024-02-07 13:05:46 +0530

    files

---
+++ python/pythondemo/functiondemo.py
@@ -0,0 +1,24 @@
+def addition():
+    a=1+1
+    print(a)
+    if a>3:
+        print("a is 3")
+        x="SNEGA DURGA THARUN THANANIYA"
+        for x1 in x:
+            print(x1)
+        x=x.split(" ")    
+        for x2 in x:
+            print(x2)
+    elif a==2:
+        print("a is 6")
+        x="TAMIL SELVI"
+        for x3 in x:
+            print(x3)
+        x=x.split(" ")
+        for x4 in x:
+            print(x4)    
+    y=input("Enter the value:").split()
+
+
+    print(y)
+addition()
