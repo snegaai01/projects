9fd082c1a0ce85a3430a20ba3f707736ef6cb615
Snega D <Snegaai01@brightskills.com>
2024-02-12 13:42:31 +0530

    added single inheritance program

---
+++ python/singleinheritance.py
@@ -0,0 +1,12 @@
+class google:
+    def welcome(self):
+        print("welcome to google ")
+
+class gmail(google):    
+    def wel(self):
+        print("welcome  to  gmail")        
+obj=gmail()
+obj.welcome()
+obj.wel()
+   
+    
