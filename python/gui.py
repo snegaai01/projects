c7ff53afef34002b459f89fbb8bb48f5598e2784
Snega D <Snegaai01@brightskills.com>
2024-02-14 13:44:51 +0530

    modified GUI program

--- python/gui.py
+++ python/gui.py
@@ -1,6 +1,20 @@
 from tkinter import *
 
 app=Tk()
-app.title("WELCOME TO GUI PROGRAM")
+app.title("WELCOME TO GUI")
 app.geometry("600x300+500+200")
+app.config(bg="pink")
+lbltitle=Label(app,text="ADDITION")
+lbltitle.grid(row=2, column=1, padx=60 , pady=60)
+
+
+inputbox1=Entry(app,width=20)
+inputbox1.grid(row=2, column=2)
+
+lbltitle=Label(app,text="SUBTRACTION")
+lbltitle.grid(row=3, column=1, padx=50, pady=20)
+inputbox2=Entry(app,width=20)
+inputbox2.grid(row=3, column=2)
+
+
 app.mainloop()
